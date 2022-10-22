950a2d3c3328286061dafaa1bb5325af477901d7
Nick Jones <n.jones@auckland.ac.nz>
2022-10-22 06:39:45 +0000

    Updated 2 files via CloudCannon

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
+      data: '_settings',
     },
     htmlTemplateEngine: "njk",
   };
---
+++ cloudcannon.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  // Global CloudCannon configuration
+  _inputs: {
+    title: {
+      type: 'text',
+      comment: 'The title of your page.'
+    }
+  },
+  _select_data: {
+    colors: ['Red', 'Green', 'Blue']
+  },
+
+  // Base path to your site source files, same as input for Eleventy
+  source: 'src',
+
+  // The subpath your built output files are mounted at
+  base_url: '/documentation',
+
+  // Populates collections for navigation and metadata in the editor
+  collections_config: {
+    people: {
+      // Base path for files in this collection, relative to source
+      path: 'content/people',
+
+      // Whether this collection produces output files or not
+      output: true,
+
+      // Collection-level configuration
+      name: 'Personnel',
+      _enabled_editors: ['data']
+    },
+    posts: {
+      path: '_posts',
+      output: true
+    },
+    pages: {
+      name: 'Main pages'
+    }
+  },
+
+  // Generates the data for select and multiselect inputs matching these names
+  data_config: {
+    // Populates data with authors from an data file with the matching name
+    authors: true,
+    offices: true
+  },
+
+  paths: {
+    // The default location for newly uploaded files, relative to source
+    uploads: 'assets/uploads',
+
+    // The path to site data files, relative to source
+    data: 'data', // defaults to _data
+
+    // The path to site layout files, relative to source
+    layouts: '_layouts', // defaults to _includes
+
+    // The path to site include files, relative to source
+    includes: '_partials' // defaults to _includes
+  }
+};
